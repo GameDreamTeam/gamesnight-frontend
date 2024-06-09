006cffdb719ff61c19976125c5a5ddcd037b8876
Ayush <lionelayush446@gmail.com>
2024-06-09 13:03:20 +0530

    add loading screen

--- src/components/JoinGame/SubmitWords.jsx
+++ src/components/JoinGame/SubmitWords.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { API_BASE_URL } from '../../constants/api';
 import { useGameContext } from './GameContext';
 
 const SubmitWords = ({ setError, setShowError }) => {
   const { gameId, currentPlayerId, adminId } = useGameContext();
+  const [loading, setLoading] = useState(false);
   const isAdmin = currentPlayerId === adminId;
 
   const handleGoToAddPhrases = async (e) => {
     e.preventDefault();
+    setLoading(true); // Show loading screen
     try {
       await axios.patch(`${API_BASE_URL}/v0/games/${gameId}/update-state`, null, { withCredentials: true });
     } catch (e) {
@@ -19,19 +21,30 @@ const SubmitWords = ({ setError, setShowError }) => {
         setShowError(false);
         setError(''); 
       }, 1500);
+    } finally {
+      setTimeout(() => {
+        setLoading(false); // Hide loading screen after 2 seconds
+      }, 2000);
     }
   };
 
   return (
-    isAdmin && (
-      <div className="submit-word">
-        <section className="submit-words">
-          <button onClick={handleGoToAddPhrases} className="next-button">
-            🚀 Everyone's Here
-          </button>
-        </section>
-      </div>
-    )
+    <>
+      {loading && (
+        <div className="loading-screen">
+          <div className="spinner"></div>
+        </div>
+      )}
+      {isAdmin && !loading && (
+        <div className="submit-word">
+          <section className="submit-words">
+            <button onClick={handleGoToAddPhrases} className="next-button">
+              🚀 Everyone's Here
+            </button>
+          </section>
+        </div>
+      )}
+    </>
   );
 };
 
